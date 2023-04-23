da21c3c7e5c6f3ec213516e05bb3b715af181464
JimSweeneyJr <jimsweeney566@gmail.com>
2023-04-23 07:16:32 -0400

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by James Sweeney on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
